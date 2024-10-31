8787e86713e8b702fc45713561ac4b5f2693ab48
toitataiyo <124337219+toitataiyo@users.noreply.github.com>
2024-10-31 23:29:49 +0900

    layout config

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ここから追加
+Route::resource('todo', 'App\Http\Controllers\TodoController');
+Route::put('todo/done/{todo}', [App\Http\Controllers\TodoController::class, 'done'])->name('todo.done');
+Route::put('todo/undone/{todo}', [App\Http\Controllers\TodoController::class, 'undone'])->name('todo.undone');
+// ここまで追加
